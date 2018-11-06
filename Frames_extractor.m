b179b80db7c23312963fd4c8dbbaa3b67c055a11
roma-claudio <36543510+roma-claudio@users.noreply.github.com>
2018-11-06 09:39:54 +0000

    Update Frames_extractor.m

--- Frames_extractor.m
+++ Frames_extractor.m
@@ -3,13 +3,13 @@ clear all;
 close all;
 soggetto = uigetfile('Frames/Video/');
 soggetto = soggetto(1:end-4);
-path = strcat('E:\OneDrive - studenti.unimi.it\Uni\Tirocinio\TESI\Video\',soggetto,'.avi');
+path = strcat('E:\Video\',soggetto,'.avi');
 vid = VideoReader(path);
 n = vid.NumberOfFrames;
 mkdir(soggetto);
 for i=1:1:7000
    frames = read(vid,i);
    FixedFrameNumber = sprintf('%05d',i);
-   path = strcat('E:\OneDrive - studenti.unimi.it\Uni\Tirocinio\TESI\Frames\',soggetto,'\',soggetto,'_frame_',FixedFrameNumber,'.jpg')
+   path = strcat('E:\Frames\',soggetto,'\',soggetto,'_frame_',FixedFrameNumber,'.jpg')
    imwrite(frames,path);
 end
